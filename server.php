d756ad61e145924d8a653226382ce089c9fd16f2
Kenny Hartono <kenny1har@yahoo.com>
2017-11-21 05:23:13 +0000

    fix

--- server.php
+++ server.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/fortune.php';
 require_once __DIR__ . '/dbraw.php';
 require_once __DIR__ . '/updateraw.php';
 use Workerman\Worker;
+use Workerman\Protocols\Http;
 
 $pdo = new PDO('mysql:host=TFB-database;dbname=hello_world;charset=utf8', 'benchmarkdbuser', 'benchmarkdbpass', array(
   PDO::ATTR_PERSISTENT => true
@@ -13,7 +14,6 @@ $http_worker = new Worker("http://0.0.0.0:8080");
 $http_worker->count = 8;
 $http_worker->onMessage = function($connection, $data)
 {
-  print_r($_SERVER);
   if ($_SERVER['REQUEST_URI'] == '/fortune.php') {
     ob_start();
     fortune($pdo);
